0166136d5517cae43818f57cdc1358947db8446d
Alexandre Hausen <alex.hausen@gmail.com>
2015-07-16 11:10:55 -0300

    different const char* access in C

---
+++ c/const_test.c
@@ -0,0 +1,21 @@
+
+int main() {
+
+  char x = 10;
+  const char* a = &x;
+  char const* b = &x;
+  char* const c = &x;
+  const char* const d = &x;
+
+  a = &x;
+  //*a = 50;
+
+  b = &x;
+  //*b = 60;
+
+  //c = &x;
+  *c = 70;
+
+  //d = &x;
+  //*d = 80;
+}
