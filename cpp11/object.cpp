4909b6dc04958d14477925a9d1d452ba422dc46b
Alexandre Hausen <alex.hausen@gmail.com>
2015-09-30 14:37:47 -0300

    fixup! some shared_ptr wrapper

--- cpp11/object.cpp
+++ cpp11/object.cpp
@@ -1,52 +1,105 @@
 #include <cassert>
 #include <iostream>
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-template <typename T> class object {
+// fwd decl
+template <typename T> class object;
+template <typename T> int count_object(const object<T>&);
+template <typename T> T* get_object_ptr(const object<T>&);
 
+template <typename T> class object {
 public:
   object() = delete;
+  ~object() noexcept = default;
 
-  object(T* p) : px{p} {
-    assert(px != nullptr);
-  }
+  // Not required (because destructor declaration automatically removes move
+  // constructor and move assignment)
+  // object(object<T>&&) = delete;
+  // object(const object<T>&&) = delete;
+  // object<T>& operator=(object<T>&&) = delete;
 
-  T& ref() noexcept {
-    return *px;
-  }
+  explicit object(T* p) : px{ p } { assert(px != nullptr); }
 
-  T& ref() const noexcept {
-    return *px;
-  }
+  operator T&() const noexcept { return *px; }
 
-  T* get() noexcept {
-    return px.get();
-  }
+  T& operator*() const noexcept { return *px; }
 
-  T* get() const noexcept {
-    return px.get();
-  }
+  T* operator->() const noexcept { return px.get(); }
+
+  // not possible
+  // T& operator.() const noexcept { return *px; }
 
 private:
   shared_ptr<T> px;
+
+  friend int count_object<T>(const object<T>&);
+  friend T* get_object_ptr<T>(const object<T>&);
+};
+
+// auxiliary functions
+template <typename T, typename... Args> object<T> make_object(Args&&... args) {
+  return object<T>{ new T{ std::forward<Args>(args)... } };
+}
+
+template <typename T> int count_object(const object<T>& obj) {
+  return obj.px.use_count();
+}
+
+template <typename T> T* get_object_ptr(const object<T>& obj) {
+  return obj.px.get();
+}
+
+struct base {
+  virtual void f() { cout << "base" << endl; }
+};
+
+struct extended : public base {
+  virtual void f() override { cout << "extended" << endl; }
 };
 
 int main() {
+  // runtime errors:
+  // object<int> bang {nullptr};
+  // object<int> bad;
 
-  //object<int> bang{ nullptr };
+  object<int> o0{ new int{ 5 } };
+  cout << *o0 << endl;
 
-  object<int> o0{new int{5}};
-  auto o1 = object<string>{new string{"abc"}};
-  vector<object<double>> v { new double{ 0.1 }, new double { 0.2 } };
+  auto mo1 = make_object<int>(10);
+  // will actually make a copy instead of 'move' (because a destructor was
+  // declared)
+  auto mo2 = std::move(mo1);
+  cout << get_object_ptr(mo1) << ",count: " << count_object(mo1) << endl;
+  cout << *mo1 << endl;
 
-  cout << o0.ref() << endl;
-  cout << o1.ref() << endl;
+  auto o1 = object<string>{ new string{ "abc" } };
+  cout << *o1 << endl;
+
+  vector<object<double> > v{ make_object<double>(1.0),
+                             make_object<double>(2.0) };
+  cout << "{ ";
   for (const auto& o : v) {
-    cout << o.ref() << " " << o.get() << endl;
+    cout << *o << " ";
   }
+  cout << "}" << endl;
 
+  extended ext;
+  base& base_ref = ext;
+  base_ref.f();
+
+  auto o2 = make_object<extended>();
+  o2->f();
+  extended& ref2 = o2;
+  ref2.f();
+  (static_cast<extended&>(o2)).f();
+  // operator. can not be overriden in c++ (and operator T&() won't do it)
+  // o2.f();
+
+  // why? how to do it?
+  //  object<base> o3 = make_object<extended>();
+  //  o3->f();
 }
