31244a3c185eb6403b5e839bbd8c068f24aaa436
Alexandre Hausen <alex.hausen@gmail.com>
2015-11-04 18:14:39 -0200

    fixup! add require examples in ruby

--- ruby/test1.rb
+++ ruby/test1.rb
@@ -1,5 +1,5 @@
-require File.expand_path('test2.rb', File.dirname(__FILE__))
-require File.expand_path('test3/test4.rb', File.dirname(__FILE__))
+require File.expand_path('test2', File.dirname(__FILE__))
+require File.expand_path('test3/test4', File.dirname(__FILE__))
 
 f
 g
