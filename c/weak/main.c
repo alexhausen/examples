9defb4ffd252a901dee783cd3a05b7e517a72450
Alexandre Hausen <alh@certi.org.br>
2016-09-21 12:13:01 -0300

    [c] usage of weak attribute

---
+++ c/weak/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "real.h"
+
+void fun() {
+  puts("real fun");
+}
+
+int main() {
+  fun();
+  return 0;
+}
---
+++ c/weak/real.h
@@ -0,0 +1,2 @@
+#pragma weak fun
+void fun() /*__attribute__((weak))*/;
