10d24fa4b46332cdab7e6ed7c13bbeff347bce7e
Alexandre Hausen <alh@certi.org.br>
2017-01-31 16:58:54 -0200

    [c] add non-lazy implementation of CRC-16.

--- c/crc16.c
+++ c/crc16.c
@@ -20,9 +20,9 @@ uint16_t get_value_from_table(uint8_t x) {
   
   uint16_t crc = x;
   for (int bit = 8; bit != 0; --bit) {
-    bool lowbit = crc & 0x0001;
+    bool low_bit = crc & 0x0001;
     crc >>= 1;
-    if (lowbit) crc ^= CRC16_GENERATOR;
+    if (low_bit) crc ^= CRC16_GENERATOR;
   }
   table[x] = crc;
   table_init[x] = true;
@@ -39,6 +39,22 @@ uint16_t crc16(const uint8_t* data, int n) {
   return crc;
 }
 
+// non-lazy impl
+uint16_t crc16_hard(const uint8_t data[], int n) {
+    uint16_t crc = 0;
+    for (int i = 0; i < n; ++i) {
+        crc ^= data[i];
+        for (int j = 8; j != 0; --j) {
+            bool low_bit = crc & 0x0001;
+            crc >>= 1;
+            if (low_bit) crc ^= CRC16_GENERATOR;
+        }
+    }
+    return crc;
+}
+
+
+
 #define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 
 int main() {
@@ -46,29 +62,31 @@ int main() {
   const char* input1 = "abcdefghijklmnopqrstuvwxyz";
   uint16_t output1 = crc16((const uint8_t*)input1, strlen(input1));
   printf("crc '%s' = 0x%04X\n", input1, output1);
-  assert(output1 == 0x71F9); // <- 0xD175
-  //assert(output1 == 0x9C1D);
+  assert(output1 == 0x71F9);
+  assert(output1 == crc16_hard((const uint8_t*)input1, strlen(input1)));
 
   const uint8_t input2[] = { 0 };
   uint16_t output2 = crc16(input2, ARRAY_SIZE(input2));
   printf("crc 0x%04X = 0x%04X\n", input2[0], output2);
   assert(output2 == 0);
+  assert(output2 == crc16_hard(input2, ARRAY_SIZE(input2)));
 
   const uint8_t input3[] = { 1 };
   uint16_t output3 = crc16(input3, ARRAY_SIZE(input3));
   printf("crc 0x%04X = 0x%04X\n", input3[0], output3);
-  assert(output3 == 0x93D0); // <- 0xD175
-  //assert(output3 == 0xC0C1);
+  assert(output3 == 0x93D0);
+  assert(output3 == crc16_hard(input3, ARRAY_SIZE(input3)));
 
   const uint8_t input4[] = { 1 };
   uint16_t output4 = crc16(input4, 0);
   assert(output4 == 0);
+  assert(output4 == crc16(input4, 0));
 
   const uint8_t input5[] = {  0x47, 0x1, 0x5, 0x0, 0x0, 0x0, 0x0, 0x0, 0x6 };
   uint16_t output5 = crc16(input5, ARRAY_SIZE(input5));
   printf("crc (%lu) 0x%04X\n", ARRAY_SIZE(input5), output5);
-  assert(output5 == 0x610A); // <- 0xD175
-
+  assert(output5 == 0x610A);
+  assert(output5 == crc16(input5, ARRAY_SIZE(input5)));
 
   return 0;
 }
