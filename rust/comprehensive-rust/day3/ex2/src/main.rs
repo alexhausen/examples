5b13576f46172ddbccb78e0bbaac70e6d301f730
Alexandre Hausen <alexandre.hausen@3778.care>
2023-03-03 19:11:38 -0300

    Add more exercises from comprehensive rust book (day3)

---
+++ rust/comprehensive-rust/day3/ex2/src/main.rs
@@ -0,0 +1,90 @@
+// https://google.github.io/comprehensive-rust/exercises/day-3/safe-ffi-wrapper.html
+
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_imports, unused_variables, dead_code)]
+
+mod ffi {
+    use std::os::raw::{c_char, c_int, c_long, c_ulong, c_ushort};
+
+    // Opaque type. See https://doc.rust-lang.org/nomicon/ffi.html.
+    #[repr(C)]
+    pub struct DIR {
+        _data: [u8; 0],
+        _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
+    }
+
+    // Layout as per readdir(3) and definitions in /usr/include/x86_64-linux-gnu.
+    #[repr(C)]
+    pub struct dirent {
+        pub d_ino: c_long,
+        pub d_off: c_ulong,
+        pub d_reclen: c_ushort,
+        pub d_type: c_char,
+        pub d_name: [c_char; 256],
+    }
+
+    extern "C" {
+        pub fn opendir(s: *const c_char) -> *mut DIR;
+        pub fn readdir(s: *mut DIR) -> *const dirent;
+        pub fn closedir(s: *mut DIR) -> c_int;
+    }
+}
+
+use std::ffi::{CStr, CString, OsStr, OsString};
+use std::os::unix::ffi::OsStrExt;
+
+use ffi::{closedir, readdir};
+use libc::strlen;
+
+#[derive(Debug)]
+struct DirectoryIterator {
+    path: CString,
+    dir: *mut ffi::DIR,
+}
+
+impl DirectoryIterator {
+    fn new(path: &str) -> Result<DirectoryIterator, String> {
+        // Call opendir and return a Ok value if that worked,
+        // otherwise return Err with a message.
+        let path_str = CString::new(path).unwrap();
+        let dir = unsafe { ffi::opendir(path_str.as_ptr()) };
+        if dir.is_null() {
+            return Err(format!("Error opening {:?}.", path));
+        }
+        Ok(DirectoryIterator {
+            path: path_str,
+            dir,
+        })
+    }
+}
+
+impl Iterator for DirectoryIterator {
+    type Item = OsString;
+    fn next(&mut self) -> Option<OsString> {
+        // Keep calling readdir until we get a NULL pointer back.
+        let dir_entry = unsafe { ffi::readdir(self.dir) };
+        if dir_entry.is_null() {
+            return None;
+        }
+        let c_name = unsafe { CStr::from_ptr((*dir_entry).d_name.as_ptr()) };
+        let os_str = OsStr::from_bytes(c_name.to_bytes());
+        Some(os_str.to_owned())
+    }
+}
+
+impl Drop for DirectoryIterator {
+    fn drop(&mut self) {
+        // Call closedir as needed.
+        if !self.dir.is_null() {
+            if unsafe { closedir(self.dir) } != 0 {
+                panic!("Could not close {:?}", self.path);
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let iter = DirectoryIterator::new(".")?;
+    println!("files: {:#?}", iter.collect::<Vec<_>>());
+    Ok(())
+}
